9ea379b817fe97fc65406692710b94a22b206291
Christen Garcia <30610163+christengarcia@users.noreply.github.com>
2018-02-22 07:47:59 +1100

    Initial commit

---
+++ thread_pools/Program.cs
@@ -0,0 +1,19 @@
+﻿/*
+ * C# Program to Create Thread Pools
+ */
+using System;
+using System.Threading;
+
+namespace thread_pools
+{
+    class Program
+    {
+        public void Task1(object obj)
+        {
+            for (int i = 0; i <= 2; i++)
+            {
+                Console.WriteLine("Task 1 is being executed");
+            }
+        }
+    }
+}
