1e6e1e50dec6a68417a8c2e317e061964a32fa24
Christen Garcia <30610163+christengarcia@users.noreply.github.com>
2018-02-22 07:50:00 +1100

    Add Task2

--- thread_pools/Program.cs
+++ thread_pools/Program.cs
@@ -15,5 +15,13 @@ namespace thread_pools
                 Console.WriteLine("Task 1 is being executed");
             }
         }
+
+        public void Task2(object obj)
+        {
+            for (int i = 0; i <= 2; i++)
+            {
+                Console.WriteLine("Task 2 is being executed");
+            }
+        }
     }
 }
